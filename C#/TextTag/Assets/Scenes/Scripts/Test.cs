090e4f4562e625efb728b8d96139f46a9df52816
PotroChen <1400539511@qq.com>
2020-01-24 01:26:12 +0800

    基础功能实现

---
+++ C#/TextTag/Assets/Scenes/Scripts/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public GameObject Target;
+    public TextTag TextTag;
+    public Vector2 offset;
+    public Transform UIRoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Trigger()
+    {
+        TextTag textTagInstance = Object.Instantiate(TextTag);
+        textTagInstance.transform.SetParent(UIRoot);
+        textTagInstance.StartAnimation(Target, offset, "YOHO~", Camera.main);
+    }
+}
